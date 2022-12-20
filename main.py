d3ee05f925a00d2d919c585e777e04c3b3a1635d
itzrovic <120782777+itzrovic@users.noreply.github.com>
2022-12-20 16:56:51 +0100

    print(generate_lotto_numbers(4, 50)) -> # print(generate_lotto_numbers(4, 50)) print(generate_lotto_numbers(6, 6)) -> # print(generate_lotto_numbers(6, 6)) Function generate_lotto_numbers extended feature: does not return numbers twice anymore

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ def generate_lotto_numbers(count_of_num: int, max_num: int) -> list:
     rand_list = []
     for i in range(0, count_of_num):
         randnum = random.randint(1, max_num)
-        rand_list.append(randnum)
+        if randnum not in rand_list:
+            rand_list.append(randnum)
+        else:
+            while (randnum in rand_list):
+                randnum = random.randint(1, max_num)
+            rand_list.append(randnum)
     return rand_list
 
 print(generate_lotto_numbers(6, 45))
 print(generate_lotto_numbers(6, 49))
-print(generate_lotto_numbers(4, 50))
-print(generate_lotto_numbers(6, 6))
+# print(generate_lotto_numbers(4, 50))
+# print(generate_lotto_numbers(6, 6))
