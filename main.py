dd7ce036583f8db695382805298a320189e89815
itzrovic <120782777+itzrovic@users.noreply.github.com>
2022-12-20 17:43:11 +0100

    added new function lotto_probability Function recives 2 variables: a list of lotto numbers and the max number of the lottery Function lotto_probability count how many tries it takes to randomly get the same ticket ATTENTION: takes a while to count so many possibilities

--- main.py
+++ main.py
@@ -14,7 +14,28 @@ def generate_lotto_numbers(count_of_num: int, max_num: int) -> list:
             rand_list.append(randnum)
     return rand_list
 
-print(generate_lotto_numbers(6, 45))
-print(generate_lotto_numbers(6, 49))
+def lotto_probability(numbers: list[int], max_num: int) -> int: # ich habe es nicht hingekriegt zu zählen, wieviele Versuche man insgesamt braucht. Es zählt nur wie viele Versuche man braucht wenn man zufällig zieht bis das gleiche rauskommt
+    count_tries = 0
+    lottery_ticket = numbers
+    count_ticket = []
+    rand_ticket = generate_lotto_numbers(6, max_num)
+    while (rand_ticket != lottery_ticket):
+        rand_ticket = generate_lotto_numbers(6, max_num)
+        count_tries += 1
+    # for i in range(max_num):
+    #     count_ticket.append
+
+    return count_tries
+
+AT = 45
+DE = 49
+AT_lotto_numbers_list = generate_lotto_numbers(6, 45)
+DE_lotto_numbers_list = generate_lotto_numbers(6, 49)
+
+tries = lotto_probability(AT_lotto_numbers_list, AT)
+print(tries)
+
+# print(generate_lotto_numbers(6, 45))
+# print(generate_lotto_numbers(6, 49))
 # print(generate_lotto_numbers(4, 50))
 # print(generate_lotto_numbers(6, 6))
